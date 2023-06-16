67b08188b239d190c96b1f8dc13ab7f744b26c31
Kojod140 <kojod140@gmail.com>
2023-06-16 00:37:47 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * return: 0(success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingua l pizzle");
+return (0); 
